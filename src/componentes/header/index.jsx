d75816a2538257407028816d47498ad3230a7e95
Sophizada <you@example.com>
2023-10-06 19:43:10 -0300

    front finalizado

--- src/componentes/header/index.jsx
+++ src/componentes/header/index.jsx
@@ -1,26 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { getCategorias } from '../../api/api';
 import logo from "../../assets/imgs/logo/logo.png";
 import iconMenu from "../../assets/imgs/icones/icon-menu.png";
-
+import iconClose from "../../assets/imgs/icones/icon-close.png";
 import "./style.css";
 
 const Header = () => {
-    return(
-        <header>
-            <nav>
-                <div className="box-logo">
-                    <img src={logo} alt="Logo" />
-                </div>
-                <div className="box-slogan">
-                    <h1>"Onde cada livro é uma chave para a imaginação"</h1>
-                </div>
-                <div className="box-menu">
-                    <button><img src={iconMenu} alt="" /></button>
-                </div>
-            </nav>
-        </header>
-    );
-}
+  const [categorias, setCategorias] = useState([]);
+  const [menuVisivel, setMenuVisivel] = useState(false);
 
+  const toggleMenu = () => {
+    setMenuVisivel(!menuVisivel);
+  };
+
+  useEffect(() => {
+    getCategorias()
+      .then(response => {
+        setCategorias(response.data);
+      })
+      .catch(error => console.error(error));
+  }, []);
+
+  return (
+    <header className="header">
+      <nav className="nav">
+        <div className="box-logo">
+          <img src={logo} alt="Logo" />
+        </div>
+        <div className="box-slogan">
+          <h1>"Onde cada livro é uma chave para a imaginação"</h1>
+        </div>
+        <div className="box-menu">
+          <button onClick={toggleMenu}>
+            {menuVisivel ? <img src={iconClose} alt="Fechar" /> : <img src={iconMenu} alt="Menu" />}
+          </button>
+        </div>
+      </nav>
+
+      <div className={`menu-categorias ${menuVisivel ? 'menu-visivel' : ''}`}>
+        <div className="options">
+            <h3>Categorias</h3>
+            <button onClick={toggleMenu}><img src={iconClose} alt="Menu" /></button>
+        </div>
+
+        <div className="categorias">
+            {categorias.map(categoria => (
+                <a key={categoria.id} className="categoria" href="/">{categoria.name}</a>
+                
+            ))}
+        </div>
+      </div>
+    </header>
+  );
+}
 
 export default Header;
--- src/paginas/info-livro/index.jsx
+++ src/paginas/info-livro/index.jsx
@@ -1,19 +1,13 @@
-import { useState, useEffect } from "react";
-import { getLivros } from "../../api/api";
+import React from "react";
 import "./style.css";
 import Header from "../../componentes/header";
-import CardLivros from "../../componentes/cardLivro";
+// import CardLivros from "../../componentes/cardLivro";
+
+import livroImg from '../../assets/imgs/livros/a-mulher-na-janela.jpg';
+
 
 const PagInfosLivro = () => {
-    const [livros, setLivros] = useState([]);
-  
-    useEffect(() => {
-        getLivros()
-        .then(response => {
-            setLivros(response.data);
-        })
-        .catch(error => console.error(error));
-    }, []);
+    
   
     return(
         <div className="pagCategoria">
@@ -21,11 +15,18 @@ const PagInfosLivro = () => {
             <div className="box-livros">
                 <div className="box-infos-livros">
                     <div className="box-img">
-
+                            <img src={livroImg} alt="" />
                     </div>
-                    {/* {livros.map(livro => (
-                        
-                    ))} */}
+                    
+                    <div className="box-infos">
+                        <h1 className="titulo">A Mulher na Janela</h1>
+                        <p>Autor: <span>A.J. Finn</span></p>
+                        <p>Gênero <span>Suspense</span></p>
+                        <p>Lançamento: <span>2018</span></p>
+                        <p>Sinópse: <span>A história segue Anna Fox, uma mulher agorafóbica que passa os dias observando a vida de seus vizinhos pela janela. Quando testemunha algo perturbador na casa dos novos vizinhos, ela se vê envolvida em um mistério envolvendo segredos obscuros e mentiras.</span></p>
+                    </div>
+
+                    
                 </div>
             </div>
         </div>
